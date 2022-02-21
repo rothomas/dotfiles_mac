5282e527810aed2d715fdefadb2c3717c61a438f
Rob Thomas <rob@formswift.com>
2022-02-21 07:28:58 -0700

    diagnostic logging

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-
+echo 'INSTALL!'
 . `dirname $0`/.helpers/prereq_functions
 
 require curl
 
 if ! installed chezmoi; then
   chezmoi_script=`curl -fsLS chezmoi.io/get`
-  sh -c "${chezmoi_script}" -- init rothomas
+  sh -c "${chezmoi_script}" -- --apply init rothomas
 fi
 
