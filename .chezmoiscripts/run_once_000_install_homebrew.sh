09992d4d91d396673044cf8b2fb9154f4fb4072e
Rob Thomas <rob@formswift.com>
2022-02-21 07:19:14 -0700

    fix: Remove unset var in toml

--- .chezmoiscripts/run_once_000_install_homebrew.sh
+++ .chezmoiscripts/run_once_000_install_homebrew.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+. `dirname $0`/.helpers/prereq_functions
+
 homebrew_install_apt_prereqs() {
   #################################################################################################
   #   APT PLATFORMS (Debian, Ubuntu)
   #################################################################################################
-  if [[ "$(apt list build-essential procps curl file git 2>/dev/null | grep -cF '[installed]')" != "5" ]]; then
-    echo "Installing apt prerequisites (sudo required)"
-    sudo apt -y install build-essential procps curl file git
-  fi
+  require build-essential
+  require procps
+  require curl
+  require file
+  require git
 }
 
 homebrew_install_yum_prereqs() {
@@ -18,14 +21,12 @@ homebrew_install_yum_prereqs() {
     echo "Installing development tools (sudo required)"
     sudo yum groupinstall -y 'Development Tools'
   fi
-  reqs=(procps-ng curl file git)
-  if [[ "$(yum list installed procops-ng curl file git 2>/dev/null | grep -c $(uname -m))" != "4" ]]; then
-    echo "Installing yum prerequisites (sudo required)"
-    sudo yum install -y procops-ng curl file git
-  fi
+  require procps-ng
+  require curl
+  require file
+  require git
   if [[ "$ID" == "fedora" && "$(yum list installed libxcrypt-compat 2>/dev/null | grep -c $(uname -m))" != "1" ]]; then
-    echo "Installing libxcrypt-compat for Fedora (sudo required)"
-    sudo yum install -y libxcrypt-compat
+    require libxcrypt-compat
   fi
 }
 
@@ -41,11 +42,10 @@ homebrew_install_linux_prereqs() {
   fi
 }
 
-
 homebrew_install() {
   if [[ -z "$(command -v brew)" || ! "$(brew --version | head -1)" =~ "^Homebrew" ]]; then
     if [[ "$(uname)" == "Linux" ]]; then
-      install_linux_prereqs
+      homebrew_install_linux_prereqs
     fi
 
     NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
