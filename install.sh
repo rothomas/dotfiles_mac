c14f6cf5c30ba0068c4427ef03e78e53ac25ac16
Rob Thomas <rob@formswift.com>
2022-03-02 12:31:54 -0700

    feat: copy helpers to install.sh for standalone use

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-. `dirname $0`/.helpers/prereq_functions
+installed() {
+  command -v "$1" >/dev/null
+}
+
+download() {
+  local url=$1
+  if installed curl; then
+    curl -o- "$url"
+  elif installed wget; then
+    wget -qO- "$url"
+  else
+    >&2 echo "Either wget or curl must be installed. Cannot continue."
+    exit 1
+  fi
+}
 
 if ! installed chezmoi; then
-  chezmoi_script=`download chezmoi.io/get`
+  script=`download chezmoi.io/get`
   sh -c "${chezmoi_script}" -- init --verbose --apply rothomas
 fi
