b1f773e62ed3da7d62497aead5d6bf3d27549d03
Rob Thomas <rob@formswift.com>
2022-06-02 20:48:10 -0600

    feat: zsh configuration

--- install.sh
+++ install.sh
@@ -1,109 +1,81 @@
 #!/bin/sh
 
-case `uname` in
-Darwin)
-  # curl should always be installed here
-  ;;
-Linux)
-  . /etc/os-release
-  ;;
-*)
-  echo "Unsupported OS platform. Supported platforms are Linux and Darwin. Cannot continue."
-  exit 1
-  ;;
-esac
+##############################################################################
+# NOTE: We cannot assume bash in a bootstrap situation, especially in a
+#       container, so this script must use Bourne shell syntax throughout.
+#       Bash is installed ASAP in the chezmoi scripts, so except as noted
+#       bash syntax will be accepted in everything else in .chezmoiscripts.
+#
+# This script aims to be maximally robust to initial conditions. We need
+# chezmoi to already be downloaded and on PATH, or we need curl or wget so
+# we can download it, and if neither of those are installed, we need
+# to be either root or a sudoer to install curl.
+##############################################################################
+
+export PLATFORM=`uname`
+[ -f /etc/os-release ] && . /etc/os-release
+# Known os releases used to determine package manager.
+package_manager_centos=yum
+package_manager_rhel=yum
+package_manager_fedora=yum
+package_manager_ubuntu=apt
+package_manager_debian=apt
+package_manager_arch=pacman
+package_manager_alpine=apk
+# Set the platform package manager, if relevant.
+eval package_manager="\${package_manager_${ID}}" 
+
+install_curl_yum="yum update && yum install -y curl"
+install_curl_apt="apt-get update -y && apt-get install -y curl"
+install_curl_apk="apk add --update curl"
+install_curl_pacman="pacman -Sy curl"
+eval install_curl="\${install_curl_${package_manager}}"
 
 installed() {
+  # Syntactic sugar for the below.
   command -v "$1" >/dev/null
 }
 
+fail() {
+  # General exit with a message.
+  >&2 echo "${1}"
+  exit 1
+}
+
 download() {
+  # Download a URL using curl or wget, if available.
   local url=$1
-  if installed curl; then
-    curl -Lso- "$url"
-  elif installed wget; then
-    wget -qO- "$url"
-  else
-    >&2 echo "Either wget or curl must be installed. Cannot continue."
-    exit 1
+  if installed curl; then curl -Lso- "$url"
+  elif installed wget; then wget -qO- "$url"
+  else fail "Either wget or curl must be installed. Cannot continue."
   fi
-}
-
-run_privileged() {
-  if [ `whoami` = 'root' ]; then
-    $@
-  else
-    sudo $@
-  fi
-}
-
-install_curl() {
-  case $ID in
-  centos)
-    package_manager=yum
-    ;;
-  rhel)
-    package_manager=yum
-    ;;
-  fedora)
-    pakcage_manager=yum
-    ;;
-  ubuntu)
-    package_manager=apt
-    ;;
-  debian)
-    package_manager=apt
-    ;;
-  arch)
-    package_manager=pacman
-    ;;
-  alpine)
-    package_manager=apk
-    ;;
-  default)
-    echo "Unknown OS ID ${ID}. Cannot continue."
-    exit 1
-    ;;
-  esac
-  case "${package_manager}" in
-  yum)
-    run_privileged yum update
-    run_privileged yum install -y curl
-    ;;
-  apt)
-    run_privileged apt-get update -y
-    run_privileged apt-get install -y curl
-    ;;
-  pacman)
-    run_privileged pacman -Sy curl
-    ;;
-  apk)
-    run_privileged apk add --update curl
-    ;;
-  *)
-    echo "Unknown package manager. Cannot continue."
-    exit 1
-    ;;
-  esac
 }
 
 check_prerequisites() {
+  # Darwin definitely has curl available.
+  [ "${PLATFORM}" = 'Darwin' ] && return 0
+  # If it's not Darwin, it must be Linux or we don't know what to do.
+  [ "${PLATFORM}" = 'Linux' ] || fail "Unsupported OS platform: ${PLATFORM}. Cannot continue."
+  # If curl is available we're good.
   installed curl && return 0
+  # If wget is available we're good.
   installed wget && return 0
-  if [ `whoami` = 'root' ]; then
-    install_curl
-  elif command -v sudo && sudo -v; then
-    install_curl
-  else
-    echo "Cannot satisfy prerequisites. You must have curl or wget installed, or run as root, or be able to sudo."
-    exit 1
+  # Can only install curl if we know the package manager.
+  [ -z "${package_manager}" ] && "Cannot identify package manager for ${ID}. Cannot continue."
+  # If we're running as root, sudo doesn't need to exist.
+  if [ `whoami` = 'root' ]; then /bin/sh -c "${install_curl}"
+  # If we're running as non-root we'll need to be sudoers.
+  elif installed sudo && sudo -v; then sudo /bin/sh -c "${install_curl}"
+  # Out of options.
+  else fail "Cannot satisfy prerequisites. You must have curl or wget installed, or run as root, or be able to sudo."
   fi
 }
 
->&2 echo "Installing..."
-if ! installed chezmoi; then
+install_chezmoi() {
   check_prerequisites
-  >&2 echo "Installing chezmoi"
   chezmoi_script=`download chezmoi.io/get`
   sh -c "${chezmoi_script}" -- init --verbose --apply rothomas
-fi
+}
+
+installed chezmoi || install_chezmoi
+
